7d94e797fc77363f132329ace3f91c4e6c689c20
Carsten Lemm <clemm@microsoft.com>
2025-08-05 16:48:32 +0200

    cleaned up code

--- src/Client/Hubs/GroupChatHub.cs
+++ src/Client/Hubs/GroupChatHub.cs
@@ -9,16 +9,11 @@ namespace AIStreaming.Hubs
     {
         private readonly GroupAccessor _groupAccessor;
         private readonly GroupHistoryStore _history;
-        //private readonly OpenAIClient _openAI;
-        //private readonly OpenAIOptions _options;
 
-        public GroupChatHub(GroupAccessor groupAccessor, GroupHistoryStore history /*,
-                            OpenAIClient openAI, IOptions<OpenAIOptions> options */)
+        public GroupChatHub(GroupAccessor groupAccessor, GroupHistoryStore history)
         {
             _groupAccessor = groupAccessor ?? throw new ArgumentNullException(nameof(groupAccessor));
             _history = history ?? throw new ArgumentNullException(nameof(history));
-            // _openAI = openAI ?? throw new ArgumentNullException(nameof(openAI));
-            // _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
         }
 
         public async Task JoinGroup(string groupName)
@@ -39,37 +34,8 @@ namespace AIStreaming.Hubs
             {
                 throw new InvalidOperationException("Not in a group.");
             }
-
-            // if (message.StartsWith("@gpt"))
-            // {
-            //     var id = Guid.NewGuid().ToString();
-            //     var actualMessage = message.Substring(4).Trim();
-            //     var messagesIncludeHistory = _history.GetOrAddGroupHistory(groupName, userName, actualMessage);
-            //     await Clients.OthersInGroup(groupName).SendAsync("NewMessage", userName, message);
-
-            //     var chatClient = _openAI.GetChatClient(_options.Model);
-            //     var totalCompletion = new StringBuilder();
-            //     var lastSentTokenLength = 0;
-            //     await foreach (var completion in chatClient.CompleteChatStreamingAsync(messagesIncludeHistory))
-            //     {
-            //         foreach (var content in completion.ContentUpdate)
-            //         {
-            //             totalCompletion.Append(content);
-            //             if (totalCompletion.Length - lastSentTokenLength > 20)
-            //             {
-            //                 await Clients.Group(groupName).SendAsync("newMessageWithId", "ChatGPT", id, totalCompletion.ToString());
-            //                 lastSentTokenLength = totalCompletion.Length;
-            //             }
-            //         }
-            //     }
-            //     _history.UpdateGroupHistoryForAssistant(groupName, totalCompletion.ToString());
-            //     await Clients.Group(groupName).SendAsync("newMessageWithId", "ChatGPT", id, totalCompletion.ToString());
-            // }
-            // else
-            {
-                _history.GetOrAddGroupHistory(groupName, userName, message);
-                await Clients.OthersInGroup(groupName).SendAsync("NewMessage", userName, message);
-            }
+            _history.GetOrAddGroupHistory(groupName, userName, message);
+            await Clients.OthersInGroup(groupName).SendAsync("NewMessage", userName, message);
         }
     }
 }
